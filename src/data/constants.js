87a67419b88a313ddafd5780b9c7460011a05e2d
Alex Bernatowicz <bernatowicz1122@gmail.com>
2024-04-02 14:46:15 -0500

    Fixed logos for language skills

--- src/data/constants.js
+++ src/data/constants.js
@@ -13,6 +13,10 @@ import cocrop from '../images/Co-Crop.png'
 import marcinImg from '../images/Marcin.JPG'
 import danielImg from '../images/Daniel.JPG'
 import A2Z from '../images/A2Z.png'
+import git from '../images/git.svg'
+import express from '../images/express.svg'
+import NodeJS from '../images/Nodejs.svg'
+import sqlite from '../images/sqlite.svg'
 
 export const Bio = {
   
@@ -66,11 +70,11 @@ export const Bio = {
       skills: [
         {
           name: "Node JS",
-          image: "https://github.com/devicons/devicon/blob/master/icons/nodejs/nodejs-original-wordmark.svg",
+          image: NodeJS,
         },
         {
           name: "Express",
-          image: "https://github.com/devicons/devicon/blob/master/icons/express/express-original-wordmark.svg",
+          image: express,
         },
         {
             name: "MongoDB",
@@ -101,8 +105,7 @@ export const Bio = {
         },
         {
           name: "SQLite3",
-          image:
-          "https://github.com/devicons/devicon/blob/master/icons/sqlite/sqlite-original.svg",
+          image: sqlite,
       },
       ],
     },
@@ -111,8 +114,7 @@ export const Bio = {
       skills: [
         {
             name: "Git",
-            image:
-                "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+            image: git,
         },
         {
             name: "GitHub",
