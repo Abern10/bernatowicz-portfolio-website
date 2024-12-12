270462b47b8185fa7261689d9e75c7cb877c14f0
Alex Bernatowicz <bernatowicz1122@gmail.com>
2024-12-11 19:24:51 -0600

    new resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -33,7 +33,7 @@ export const Bio = {
   description:
     "As a fourth year undergraduate at the University of Illinois at Chicago (UIC) pursuing a degree in Computer Science with a concentration in Software Engineering, I am dedicated to expanding my knowledge and skills in the field of programming. With a strong foundation in a wide variety of programming languages such as C++, C, HTML/CSS, Java, JavaScript, Node.Js, Express, SQL, TypeScript, Swift and Python. In addition, I have used libraries and frameworks such as ReactJS and NextJS and Python with Flask. I am equipped with the technical expertise needed to tackle complex software engineering challenges. Throughout my academic journey, I have demonstrated a strong work ethic and a passion for learning. I am committed to staying up-to-date with the latest developments in the field of computer science and software engineering, and I am always eager to take on new projects and challenges. I am excited about the opportunities that lie ahead in the field of computer science and software engineering, and I am eager to leverage my skills and expertise to make a meaningful contribution to the industry.",
   github: "https://github.com/Abern10",
-  resume: "https://drive.google.com/file/d/1Agwrl4BMIxkouvHYYabZbdHgGkdHQEci/view?usp=share_link",
+  resume: "https://drive.google.com/file/d/1KknCoYDd0OQPEibVcgT1fwTXjyIOGNZX/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/alexander-bernatowicz-931462224/",
 };
 
